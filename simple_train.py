68716ddc1ac71640f054a7a1331853f80da7560e
matech96 <gangoly96@gmail.com>
2019-06-29 21:07:05 +0200

    analysis and long run

--- simple_train.py
+++ simple_train.py
@@ -4,6 +4,12 @@ from train import train_cifar10
 
 experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
                         project_name="general", workspace="matech96")
-experiment.set_name('long train for early stopping')
+experiment.set_name('long train for early stopping#3')
 
-train_cifar10(batch_size=64, learning_rate=0.001, epochs=1500, experiment=experiment)
+train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment)
+
+experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
+                        project_name="general", workspace="matech96")
+experiment.set_name('long train for early stopping#4')
+
+train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment)

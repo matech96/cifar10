49ec65a83b6a1c3d408a19b3aa9db77a609ae938
matech96 <gangoly96@gmail.com>
2019-07-05 23:34:30 +0200

    train dev and test sets

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import os
+from sklearn.utils import shuffle
 
 from comet_ml import Experiment
 import keras
@@ -170,9 +171,25 @@ def train_cifar10(batch_size: int, learning_rate: float, epochs: int, experiment
     log_path = os.path.join(save_dir, log_name)
     # The data, split between train and test sets:
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    # Convert class vectors to binary class matrices.
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+    X = np.concatenate((x_train, x_test), axis=0)
+    Y = np.concatenate((y_train, y_test), axis=0)
+    Y = keras.utils.to_categorical(Y, num_classes)
+
+    n_data_points = X.shape[0]
+    random_indecies = shuffle(np.arange(n_data_points), random_state=42)
+    X = X[random_indecies,]
+    Y = Y[random_indecies,]
+    X = X.astype('float32') / 255.0
+    Y = Y.astype('float32')
+    border_train = int(n_data_points * 0.7)
+    border_dev = int(n_data_points * 0.9)
+    x_train = X[:border_train, ]
+    y_train = Y[:border_train, ]
+    x_dev = X[border_train:border_dev, ]
+    y_dev = Y[border_train:border_dev, ]
+    x_test = X[border_dev:, ]
+    y_test = Y[border_dev:, ]
+
     input_shape = x_train.shape[1:]
     model = model_fnc(input_shape, num_classes, regularization)
     opt = Adam(lr=learning_rate)
@@ -182,23 +199,32 @@ def train_cifar10(batch_size: int, learning_rate: float, epochs: int, experiment
     plot_model(model, model_plot_file_name, show_shapes=True)
     experiment.log_image(model_plot_file_name)
     os.remove(model_plot_file_name)
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
     csv_cb = CSVLogger(log_path)
     early_stopping = EarlyStopping('val_acc', patience=250, restore_best_weights=True, verbose=2)
     callbacks = [csv_cb, early_stopping]
     model.fit(x_train, y_train,
               batch_size=batch_size,
               epochs=epochs,
-              validation_data=(x_test, y_test),
+              validation_data=(x_dev, y_dev),
               shuffle=True,
               callbacks=callbacks,
               verbose=2)
     model.save(model_path)
+    scores = model.evaluate(x_train, y_train, verbose=2)
+    print('Train loss:', scores[0])
+    print('Train accuracy:', scores[1])
+    experiment.log_metrics({"loss": scores[0],
+                            "acc": scores[1]}, prefix="train")
+
     experiment.log_asset(model_path)
-    # Score trained model.
+    scores = model.evaluate(x_dev, y_dev, verbose=2)
+    print('Dev loss:', scores[0])
+    print('Dev accuracy:', scores[1])
+    experiment.log_metrics({"loss": scores[0],
+                            "acc": scores[1]}, prefix="dev")
+
     scores = model.evaluate(x_test, y_test, verbose=2)
     print('Test loss:', scores[0])
     print('Test accuracy:', scores[1])
+    experiment.log_metrics({"loss": scores[0],
+                            "acc": scores[1]}, prefix="test")

7c3b7fc84a27250f76bd2e7da42a779a192a8211
matech96 <gangoly96@gmail.com>
2019-07-05 10:13:41 +0200

    bugfix

--- simple_train.py
+++ simple_train.py
@@ -3,12 +3,11 @@ from comet_ml import Experiment
 from train import train_cifar10, \
     get_model, get_model_same, get_model_3block, get_model_4block, get_model_5block
 
-experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
-                        project_name="architecture testing", workspace="matech96")
-
 model_fncs = [get_model, get_model_same, get_model_3block, get_model_4block, get_model_5block]
 experiment_names = ['base', 'same', '3block', '4block', '5block']
 
 for name, model_fnc in zip(experiment_names, model_fncs):
+    experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
+                            project_name="architecture testing", workspace="matech96")
     experiment.set_name(name)
     train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment)

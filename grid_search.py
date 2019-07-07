d3e937369fd938d445f68fd2f82c15c83cbf0e7c
matech96 <gangoly96@gmail.com>
2019-07-07 21:37:11 +0200

    5block batch size and learning rate

--- grid_search.py
+++ grid_search.py
@@ -1,25 +1,23 @@
 from comet_ml import Optimizer
-from train import train_cifar10
+from train import train_cifar10, get_model_5block
 from keras import regularizers
 
 config = {
     "algorithm": "grid",
-    "name": "L1 and L2 regularization",
+    "name": "5block batch size and learning rate",
 
     "spec": {
         "metric": "acc",
     },
 
     "parameters": {
-        "l1": {"type": "discrete", "values": [1, 0.1, 0.01, 0.001, 0.0001]},
-        "l2": {"type": "discrete", "values": [1, 0.1, 0.01, 0.001, 0.0001]},
+        "batch_size": {"type": "discrete", "values": [32, 64, 128]},
+        "learning_rate": {"type": "discrete", "values": [0.01, 0.001, 0.0001]},
     },
 }
-optimizer = Optimizer(config, api_key="cgss7piePhyFPXRw1J2uUEjkQ", project_name="cifar10_l1_l2")
+optimizer = Optimizer(config, api_key="cgss7piePhyFPXRw1J2uUEjkQ", project_name="cifar10_block5_bs_lr")
 
 for experiment in optimizer.get_experiments():
-    l1 = experiment.get_parameter("l1")
-    l2 = experiment.get_parameter("l2")
-    epochs = 10
-    train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment,
-                  regularization=regularizers.l1_l2(l1=l1, l2=l2))
+    batch_size = experiment.get_parameter("batch_size")
+    learning_rate = experiment.get_parameter("learning_rate")
+    train_cifar10(batch_size=batch_size, learning_rate=learning_rate, epochs=1000, experiment=experiment, model_fnc=get_model_5block)

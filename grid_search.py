60648049d64c5d09993ae9ea020425a726723e46
matech96 <gangoly96@gmail.com>
2019-07-10 23:54:29 +0200

    refine cifar10-06-dropout

--- grid_search.py
+++ grid_search.py
@@ -1,5 +1,5 @@
 from comet_ml import Optimizer
-from train import train_cifar10, get_model_5block
+from train import train_cifar10, get_model
 from keras import regularizers
 
 config = {
@@ -11,11 +11,13 @@ config = {
     },
 
     "parameters": {
-        "dropout_rate": {"type": "discrete", "values": [0, 0.25, 0.5, 0.75, 0.9]},
+        "dropout_rate": {"type": "discrete", "values": [0.55, 0.6, 0.65, 0.7]},
     },
 }
 optimizer = Optimizer(config, api_key="cgss7piePhyFPXRw1J2uUEjkQ", project_name="cifar10_dropout")
 
 for experiment in optimizer.get_experiments():
     dropout_rate = experiment.get_parameter("dropout_rate")
-    train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment, dropout_rate=dropout_rate)
+    experiment.set_name(str(dropout_rate))
+    model = get_model(dropout_rate=dropout_rate)
+    train_cifar10(batch_size=64, learning_rate=0.001, epochs=1000, experiment=experiment, model=model)

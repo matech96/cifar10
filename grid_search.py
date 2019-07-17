2666a5a05532ca4cb5eeb47f97111d72ec21165e
matech96 <gangoly96@gmail.com>
2019-07-17 11:13:21 +0200

    cifar10-14-filling

--- grid_search.py
+++ grid_search.py
@@ -1,4 +1,6 @@
 from comet_ml import Optimizer
+from keras.preprocessing.image import ImageDataGenerator
+
 from train import train_cifar10
 from models import get_model
 
@@ -11,14 +13,16 @@ config = {
     },
 
     "parameters": {
-        "batch_size": {"type": "discrete",
-                       "values": list(range(34, 48)) + list(range(50, 64))},
+        "fill_mode": {"type": "categorical",
+                      "values": ['constant', 'nearest', 'reflect', 'wrap']},
     },
 }
-optimizer = Optimizer(config, api_key="cgss7piePhyFPXRw1J2uUEjkQ", project_name="cifar10-11-bs-power2")
+optimizer = Optimizer(config, api_key="cgss7piePhyFPXRw1J2uUEjkQ", project_name="cifar10-14-filling")
 
 for experiment in optimizer.get_experiments():
-    batch_size = experiment.get_parameter("batch_size")
-    experiment.set_name(batch_size)
+    fill_mode = experiment.get_parameter("fill_mode")
+    experiment.set_name(fill_mode)
     model = get_model()
-    train_cifar10(batch_size=batch_size, learning_rate=0.001, epochs=10, experiment=experiment, model=model)
+    training_datagen = ImageDataGenerator(width_shift_range=0.15, fill_mode=fill_mode)
+    train_cifar10(batch_size=64, learning_rate=0.001, epochs=10000, experiment=experiment, model=model,
+                  training_datagen=training_datagen)

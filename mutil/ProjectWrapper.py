50d0bedfa62a52eba8554f64aecb3874b88556af
matech96 <gangoly96@gmail.com>
2019-07-20 21:34:07 +0200

    projectwrapper read csvs

--- mutil/ProjectWrapper.py
+++ mutil/ProjectWrapper.py
@@ -1,13 +1,15 @@
 import comet_ml
+import pandas as pd
 from typing import Callable, TypeVar, List
 
 T = TypeVar('T')
 
 
 class ProjectWrapper:
-    def __init__(self, experiment_name: str):
+    def __init__(self, project_name: str):
         self.comet_api = comet_ml.API(api_key="cgss7piePhyFPXRw1J2uUEjkQ", rest_api_key="G9ts5ZxTui6k0ruDTtoQpqCkI")
-        self.experiments = self.comet_api.get(experiment_name)
+        self.experiments = self.comet_api.get(project_name)
+        self.name = project_name
 
     def get_metrics(self, metrics_name: str, class_constructor: Callable[[str], T]) -> List[T]:
         return [class_constructor(self.comet_api.get_experiment_metrics(experiment.key, metrics_name)[0]) for experiment
@@ -16,3 +18,6 @@ class ProjectWrapper:
     def get_parameter(self, parameter_name: str, class_constructor: Callable[[str], T]) -> List[T]:
         return [class_constructor(self.comet_api.get_experiment_parameters(experiment.key, parameter_name)[0]) for
                 experiment in self.experiments]
+
+    def get_csvs(self) -> List[pd.DataFrame]:
+        return [pd.read_csv('results/{}.csv'.format(e.key)) for e in self.experiments]

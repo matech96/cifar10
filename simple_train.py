352d9b92a7032f94cf90ef0d6e17c22e0cf4ab08
matech96 <gangoly96@gmail.com>
2019-07-23 21:50:24 +0200

    cifar10-21-multi-aug

--- simple_train.py
+++ simple_train.py
@@ -4,13 +4,23 @@ from keras.preprocessing.image import ImageDataGenerator
 from train import train_cifar10
 from models import get_model
 
+parameters = {
+    'horizontal_flip': True,
+    'vertical_flip': False,
+    'width_shift_range': 0.15,
+    'rotation_range': 0.0,
+    'zoom_range': 0.0,
+    'zca_whitening': False,
+    'brightness_range': None,
+    'shear_range': 0.0}
+
 experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
-                        project_name="cifar10-17-whitening", workspace="matech96")
-experiment.set_name("1")
-zca_whitening = True
-experiment.log_parameter("zca_whitening", zca_whitening)
+                        project_name="cifar10-21-multi-aug", workspace="matech96")
+experiment.set_name("flip-shift")
+experiment.log_parameters(parameters)
+
 model = get_model()
 preprocessing_fnc = lambda x: x.astype('float32') / 255.0
-training_datagen = ImageDataGenerator(zca_whitening=zca_whitening, preprocessing_function=preprocessing_fnc)
+training_datagen = ImageDataGenerator(preprocessing_function=preprocessing_fnc, **parameters)
 train_cifar10(batch_size=64, learning_rate=0.001, epochs=10000, experiment=experiment, model=model,
               training_datagen=training_datagen)

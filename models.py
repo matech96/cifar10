86dcf43ef144c2f4fdb4c90fafd0e471ebee9452
matech96 <gangoly96@gmail.com>
2019-07-24 22:52:09 +0200

    batchnorm

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from keras import regularizers, Sequential
-from keras.layers import Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense
+from keras.layers import Conv2D, Activation, MaxPooling2D, Dropout, Flatten, Dense, BatchNormalization
 
 
 def get_model(regularization: regularizers.Regularizer = None, dropout_rate: float = 0.5,
@@ -144,6 +144,50 @@ def get_model_5block(regularization: regularizers.Regularizer = None, dropout_ra
 
     model.add(Flatten())
     model.add(Dense(512, kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(Activation('relu'))
+    model.add(Dropout(dropout_rate))
+    model.add(Dense(10, kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(Activation('softmax'))
+    return model
+
+
+def get_model_5block_bn(regularization: regularizers.Regularizer = None, dropout_rate: float = 0.5,
+                     initialization: str = 'VarianceScaling'):
+    model = Sequential()
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(32, 32, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(64, (3, 3), padding='same', kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(128, (3, 3), padding='same', kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(256, (3, 3), padding='same', kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Conv2D(512, (3, 3), padding='same', kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(dropout_rate))
+
+    model.add(Flatten())
+    model.add(Dense(512, kernel_regularizer=regularization, kernel_initializer=initialization))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Dropout(dropout_rate))
     model.add(Dense(10, kernel_regularizer=regularization, kernel_initializer=initialization))
--- simple_train.py
+++ simple_train.py
@@ -8,19 +8,30 @@ parameters = {
     'horizontal_flip': True,
     'vertical_flip': False,
     'width_shift_range': 0.15,
+    'height_shift_range': 0.15,
     'rotation_range': 0.0,
     'zoom_range': 0.0,
     'zca_whitening': False,
-    'brightness_range': None,
+    'brightness_range': 0.2,
     'shear_range': 0.0}
 
 experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
                         project_name="cifar10-21-multi-aug", workspace="matech96")
-experiment.set_name("flip-shift")
+experiment.set_name("flip-shift-wh-rotate")
 experiment.log_parameters(parameters)
 
 model = get_model()
 preprocessing_fnc = lambda x: x.astype('float32') / 255.0
 training_datagen = ImageDataGenerator(preprocessing_function=preprocessing_fnc, **parameters)
+train_cifar10(batch_size=64, learning_rate=0.001, epochs=10000, experiment=experiment, model=model,
+              training_datagen=training_datagen)
+
+experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
+                        project_name="cifar10-22-batchnorm", workspace="matech96")
+experiment.set_name("default")
+
+model = get_model()
+preprocessing_fnc = lambda x: x.astype('float32') / 255.0
+training_datagen = ImageDataGenerator(preprocessing_function=preprocessing_fnc)
 train_cifar10(batch_size=64, learning_rate=0.001, epochs=10000, experiment=experiment, model=model,
               training_datagen=training_datagen)

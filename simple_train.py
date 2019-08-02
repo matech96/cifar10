65d8829cc936942f200270929695d9eec8308c53
matech96 <gangoly96@gmail.com>
2019-08-02 17:58:15 +0200

    cyclic_linear_flip_shift_300

--- simple_train.py
+++ simple_train.py
@@ -6,18 +6,18 @@ from mutil.CyclicLearningRate import TentCyclicLearningRate
 from mutil.LearningRateDecay import PolynomialDecay
 from train import train_cifar10
 
-for epochs in [100, 200, 300, 400]:
-    experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
-                            project_name="cifar10-23-lr-schedule", workspace="matech96")
-    experiment.set_name("cyclic_tent_linear_{}".format(epochs))
-    learning_rate = 0.001
-    n_resets = 4
+epochs = 300
+experiment = Experiment(api_key="cgss7piePhyFPXRw1J2uUEjkQ",
+                        project_name="cifar10-21-multi-aug", workspace="matech96")
+experiment.set_name("cyclic_linear_flip_shift{}".format(epochs))
+learning_rate = 0.001
+n_resets = 4
 
-    model = get_model()
-    preprocessing_fnc = lambda x: x.astype('float32') / 255.0
-    training_datagen = ImageDataGenerator(preprocessing_function=preprocessing_fnc)
-    scheduler = TentCyclicLearningRate(PolynomialDecay(max_epochs=int(epochs/2), init_alpha=learning_rate, power=1),
-                                       reset_epoch=epochs)
-    train_cifar10(batch_size=64, learning_rate=learning_rate, epochs=epochs * n_resets, experiment=experiment,
-                  model=model,
-                  training_datagen=training_datagen, scheduler=scheduler)
+model = get_model()
+preprocessing_fnc = lambda x: x.astype('float32') / 255.0
+training_datagen = ImageDataGenerator(preprocessing_function=preprocessing_fnc, horizontal_flip=True,
+                                      width_shift_range=0.15)
+scheduler = PolynomialDecay(max_epochs=epochs, init_alpha=learning_rate, power=1)
+train_cifar10(batch_size=64, learning_rate=learning_rate, epochs=epochs * n_resets, experiment=experiment,
+              model=model,
+              training_datagen=training_datagen, scheduler=scheduler)
